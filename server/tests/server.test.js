4644e3cae30a6b880d4b205f031939ceb1c55365
Uno Karlsson <uno.carlsson@gmail.com>
2018-07-08 23:29:57 +0200

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,17 @@ const request = require('supertest');
 const {app}   = require('./../server');
 const {Todo}  = require('./../models/todo');
 
+const todos = [{
+    text: 'First test todo'
+},{
+    text: 'Second test todo'
+}];
+
 // Clear all todos before each test (it)
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos',() => {
@@ -23,7 +31,7 @@ describe('POST /todos',() => {
                 if(error) {
                     return done(error);
                 }
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -43,9 +51,21 @@ describe('POST /todos',() => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((error) => done(error));
             });
+    });
+});
+
+describe('GET /todos',() => {
+    it('Should get all todos',(done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((respons) => {
+                expect(respons.body.todos.length).toBe(2);
+            })
+            .end(done);
     });
 });
