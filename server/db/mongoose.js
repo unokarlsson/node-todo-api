8e38ebc8a2e807662f4ca0d17146bd2d2509a08a
Uno Karlsson <uno.carlsson@gmail.com>
2018-07-13 22:28:02 +0200

    Added config of MONGOURL

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
 
+// NOTE: The environment variable MONGOURL need to be set!!!
+// Please check the mongourl.txt file.
+
+
+console.log('MONGOURL',process.env.MONGOURL);
+
+const MONGO_URL = process.env.MONGOURL || 'mongodb://todo:543TODO678-@ds235181.mlab.com:35181/node-todo-api';
+console.log('MONGO_URL',MONGO_URL);
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(MONGO_URL);
 // TODO: How to use the cloude mongodb in a smothe way
 // mongodb://<dbuser>:<dbpassword>@ds235181.mlab.com:35181/node-todo-api
 
