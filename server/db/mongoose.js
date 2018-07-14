ed76c58bf3abdf84aae400b1471a0129ab7b9e28
Uno Karlsson <uno.carlsson@gmail.com>
2018-07-14 13:49:34 +0200

    Setup separate test and development environments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,15 +3,10 @@ const mongoose = require('mongoose');
 // NOTE: The environment variable MONGOURL need to be set!!!
 // Please check the mongourl.txt file.
 
-
-console.log('MONGOURL',process.env.MONGOURL);
-
-const MONGO_URL = process.env.MONGOURL || 'mongodb://todo:543TODO678-@ds235181.mlab.com:35181/node-todo-api';
-console.log('MONGO_URL',MONGO_URL);
+const MONGODB_URI = process.env.MONGODB_URI;
+console.log('MONGODB_URI',MONGODB_URI);
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URL);
-// TODO: How to use the cloude mongodb in a smothe way
-// mongodb://<dbuser>:<dbpassword>@ds235181.mlab.com:35181/node-todo-api
+mongoose.connect(MONGODB_URI);
 
 module.exports = {mongoose};
