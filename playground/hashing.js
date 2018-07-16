4c03467d7736e823709295cc7fac8ff0268172bf
Uno Karlsson <uno.carlsson@gmail.com>
2018-07-16 12:06:06 +0200

    Hash user password before storing it

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,36 @@
-const {SHA256} = require('crypto-js');
-const jwt = require('jsonwebtoken');
+const {SHA256}  = require('crypto-js');
+const jwt       = require('jsonwebtoken');
+const bcrypt    = require('bcryptjs');
 
-var data = {
-    id: 10
-};
-var token = jwt.sign(data,'123abc');
-console.log(token);
-// The hash consists of three parts separated with dot. Header, payload, signature - jwt.io
+// Create the password hash value
+var password = '123abc!';
+bcrypt.genSalt(10,(error,salt) => {
+    bcrypt.hash(password,salt,(err,hash) => {
+        console.log('password hash',hash);
+    });
+});
 
-var decoded = jwt.verify(token,'123abc')
-console.log('decoded',decoded);
+var hashedPassword = '$2a$10$.iik/g7cFwRxWq4uqqT86e/PTlPGyEmf66M9zB3G7tjEPayngeoCW';
+
+bcrypt.compare(password,hashedPassword, (error,result) => {
+    console.log(result);
+});
+
+bcrypt.compare('234',hashedPassword, (error,result) => {
+    console.log(result);
+});
+
+
+
+// var data = {
+//     id: 10
+// };
+// var token = jwt.sign(data,'123abc');
+// console.log(token);
+// // The hash consists of three parts separated with dot. Header, payload, signature - jwt.io
+
+// var decoded = jwt.verify(token,'123abc')
+// console.log('decoded',decoded);
 
 // var message = 'I am user number 3';
 // var hash    = SHA256(message).toString();
