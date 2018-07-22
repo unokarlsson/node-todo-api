502973f21fca81b8ca2dcadf75f40e182daef194
Uno Karlsson <uno.carlsson@gmail.com>
2018-07-22 23:26:04 +0200

    Add POST /usewrs/login route

--- server/server.js
+++ server/server.js
@@ -114,7 +114,19 @@ app.post('/users',(request,response) => {
 
 app.get('/users/me',authenticate,(request,response) => {
     response.send(request.user);
-})
+});
+
+app.post('/users/login',(request,response) => {
+    var body = _.pick(request.body,['email','password']);
+    
+    User.findByCredentials(body.email,body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            response.header('x-auth',token).send(user);
+        });
+    }).catch((error) => {
+        response.status(400).send();
+    });
+});
 
 app.listen(port,() => {
     console.log(`Started on ${port}`);
